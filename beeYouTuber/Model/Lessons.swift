88b85b7a8d335f32d0bceea9c053cd08a8126e9b
Furkan Erzurumlu <furkanerzurumlu99@yahoo.com>
2022-04-10 23:12:57 +0300

    Model , Service add

---
+++ beeYouTuber/Model/Lessons.swift
@@ -0,0 +1,9 @@
+//
+//  Lessons.swift
+//  beeYouTuber
+//
+//  Created by Furkan Erzurumlu on 10.04.2022.
+//
+
+import Foundation
+
--- beeYouTuber/Services/Networking.swift
+++ beeYouTuber/Services/Networking.swift
@@ -4,6 +4,15 @@
 //
 //  Created by Furkan Erzurumlu on 4.03.2022.
 //
-
+import UIKit
 import Alamofire
 
+class AlamofireNetworking {
+    var response : Lessons?
+    
+    func fetchLessons(){
+        AF.request("https://furkansandal.com/youtuber_app/show_json.php")
+            .validate()
+            .responseDecodable(of: Lessons.self)
+    }
+}
