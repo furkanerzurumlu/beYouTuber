020ad60c4ead8b91d1da47e81a0f11f244f9e032
Furkan Erzurumlu <furkanerzurumlu99@yahoo.com>
2022-04-14 01:59:55 +0300

    build HomepageVM

---
+++ beeYouTuber/ViewController/Home/HomepageVM.swift
@@ -0,0 +1,16 @@
+//
+//  HomepageVM.swift
+//  beeYouTuber
+//
+//  Created by Furkan Erzurumlu on 14.04.2022.
+//
+
+import Foundation
+
+protocol HomepageVMDelegate{
+    var delegate: HomepageVMDelegateOutputs? {get set}
+}
+
+protocol HomepageVMDelegateOutputs: AnyObject{
+    
+}
